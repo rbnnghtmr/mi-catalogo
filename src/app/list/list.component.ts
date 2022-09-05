67d429d22c71c3ab67230b89147233399690955c
Jesús Zamarrón <jesus_kickflip@outlook.com>
2022-09-05 16:21:02 -0500

    E2 A#3

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AUTOMOVILES } from '../data';
+import { Automovil } from '../models';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  autos: Automovil[] | undefined;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.autos = AUTOMOVILES;
+  }
+
+}
